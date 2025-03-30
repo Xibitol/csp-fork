ac900bc46f8592ee1a1360269081f299aeae7f92
Xibitol <xibitol@pimous.dev>
2025-03-30 15:20:43 +0200

    Created btest target and exec for benchmarking.

---
+++ csp/btest/csp-btest.c
@@ -0,0 +1,17 @@
+/**
+ * @file csp-btest.h
+ * Entry point of CSP lib's benchmarking program.
+ *
+ * @author Xibitol <xibitol@pimous.dev>
+ * @date 2025
+ * @copyright GNU Lesser General Public License v3.0
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+	printf("Hello from the benchmarking program!\n");
+
+	return EXIT_SUCCESS;
+}
