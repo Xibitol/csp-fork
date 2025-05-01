cf936b4c18b52d85f43763b9639fa4f96deefd53
vgegaden <valentin.gegaden@etudiant.univ-lr.fr>
2025-05-01 18:02:03 +0200

    upgrade version of cmake_minimum_required

---
+++ csp/src/solver/csp-solver-fc.h
@@ -0,0 +1,25 @@
+/**
+ * @file csp-solver-fc.h
+ * Defines functions to solve a CSP problem using backtracking algorithm with forward checking.
+ *
+ * @author vgegaden
+ * @date 30/04/2025
+ */
+
+ #pragma once
+
+ #include <stdbool.h>
+ #include <stddef.h>
+ 
+ #include "core/csp-problem.h"
+ 
+ // PUBLIC
+ // Function types
+ typedef bool (*CSPConsistentFC)(const CSPProblem *, const size_t *, const void *, size_t, bool **);
+ 
+ // Functions
+ bool csp_problem_backtrack_fc(const CSPProblem *csp, size_t *values, 
+                              const void *data, size_t index, 
+                              CSPConsistentFC is_consistent, bool **domains);
+ bool csp_problem_solve_fc(const CSPProblem *csp, size_t *values, 
+                          const void *data, CSPConsistentFC is_consistent);
