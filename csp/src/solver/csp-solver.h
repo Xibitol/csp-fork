3895d64ff5e9973dc36732eb8ca1c28f5796daf7
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-04 11:45:26 +0200

    Separated DataChecklist and ValueChecklist

--- csp/src/solver/csp-solver.h
+++ csp/src/solver/csp-solver.h
@@ -19,14 +19,23 @@
 #include <core/csp-constraint.h>
 
 /**
- * Get the list of constraints to verify for the current variable to know if the CSPProblem is consistent.
+ * Get the list of value constraints to verify for the current variable to know if the CSPProblem is consistent.
  * @note This function is used by #csp_problem_is_consistent.
  * @param csp The CSP problem.
  * @param checklist Array to store the list of constraints to verify.
  * @param amount Pointer to size_t to store the number of constraints to verify.
  * @param index The index of the current variable.
  */
-typedef void CSPChecklist(const CSPProblem *csp, CSPConstraint** checklist, size_t* amount, size_t index);
+typedef void CSPValueChecklist(const CSPProblem *csp, CSPConstraint** checklist, size_t* amount, size_t index);
+
+/**
+ * Get the list of data constraints to verify for the current variable to know if the CSPProblem is consistent.
+ * @param csp The CSP problem.
+ * @param checklist Array to store the list of constraints to verify.
+ * @param amount Pointer to size_t to store the number of constraints to verify.
+ * @param index The index of the current variable.
+ */
+typedef void CSPDataChecklist(const CSPProblem *csp, CSPConstraint** checklist, size_t* amount, size_t index);
 
 /** Verify if the CSP problem is consistent at the specified index.
  * @param csp The CSP problem to verify.
@@ -38,19 +47,19 @@ typedef void CSPChecklist(const CSPProblem *csp, CSPConstraint** checklist, size
  * @pre The csp library is initialised.
  */
 extern bool csp_problem_is_consistent(const CSPProblem *csp,
-	size_t *values, const void *data, size_t index, CSPChecklist* checklist
+	size_t *values, const void *data, size_t index, CSPValueChecklist* checklist
 );
 
 /** Solve the CSP problem using backtracking.
  * @param csp The CSP problem to solve.
  * @param values The values of the variables.
  * @param data The data to pass to the check function.
- * @param dataChecklist A pointer to function to get the list of constraints affected by the contents of data for the current variable.
  * @param checklist A pointer to function to get the list of necessary constraints for the current variable.
+ * @param dataChecklist A pointer to function to get the list of constraints affected by the contents of data for the current variable.
  * @param benchmark pointer to Node counter for benchmarking, NULL if no benchmarking required
  * @return true if the CSP problem is solved, false otherwise.
  * @pre The csp library is initialised.
  * @post The values are assigned to the solution.
  */
 extern bool csp_problem_solve(const CSPProblem *csp,
-	size_t *values, const void *data, CSPChecklist* checklist, size_t* benchmark);
+	size_t *values, const void *data, CSPValueChecklist* checklist, CSPDataChecklist* dataChecklist, size_t* benchmark);
