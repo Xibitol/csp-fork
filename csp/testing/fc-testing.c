dcf14544119e2c7acdc7b1a988b90fb5640c3238
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-04 11:47:32 +0200

    Improved troubleshooting setup

--- csp/testing/fc-testing.c
+++ csp/testing/fc-testing.c
@@ -3,12 +3,59 @@
 //
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "../btest/load-new-sudoku.h"
 #include "../btest/solve-queens.h"
 #include "../btest/solve-sudoku.h"
 
+void save_puzzle_as_bytes(const size_t* puzzle, const char* filename) {
+	FILE* file = fopen(filename, "wb");
+	if (file == NULL) {
+		perror("fopen");
+		return;
+	}
+
+	fwrite(puzzle, sizeof(size_t), 81, file);
+	fclose(file);
+}
+
+size_t* load_puzzle_from_bytes(const char* filename) {
+	FILE* file = fopen(filename, "rb");
+	if (file == NULL) {
+		perror("fopen");
+		return NULL;
+	}
+
+	size_t* puzzle = malloc(81 * sizeof(size_t));
+	if (puzzle == NULL) {
+		perror("malloc");
+		fclose(file);
+		return NULL;
+	}
+
+	fread(puzzle, sizeof(size_t), 81, file);
+	fclose(file);
+	return puzzle;
+}
+
 int main(void) {
-	solve_queens(10, "test-fc-queens.txt", true, false);
-	// solve_sudoku(load_new_sudoku(10, 1)[0], "test-fc-sudoku.txt", false);
+	// solve_queens(10, "test-fc-queens.txt", false, false);
+
+	// size_t** sudoku = load_new_sudoku(5, 1);
+
+	// Save the puzzle as bytes
+	// save_puzzle_as_bytes(sudoku[0], "current-puzzle.bin");
+
+	// Load the puzzle back
+	size_t* loaded_puzzle = load_puzzle_from_bytes("current-puzzle.bin");
+
+	// Use the loaded puzzle (e.g., solve it)
+	solve_sudoku(loaded_puzzle, "test-fc-sudoku.txt", false);
+
+	// free(sudoku[0]);
+	// free(sudoku);
+	free(loaded_puzzle);
+	return 0;
 }
