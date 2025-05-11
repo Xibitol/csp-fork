b647279513a27f07c331a70ca643f2f64dad9c32
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-11 20:22:02 +0200

    Initial commit, copy of fc for now

---
+++ csp/src/solver/csp-solver-ovars.c
@@ -0,0 +1,148 @@
+/**
+* @file csp-solver-ovars.c
+ * Library CSP solving functions with forward checking + variable heuristics
+ *
+ * @author agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
+ * @date 2025
+ * @copyright GNU Lesser General Public License v3.0
+ */
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "core/csp-constraint.h"
+#include "core/csp-lib.h"
+#include "core/csp-problem.h"
+#include "solver/csp-solver.h"
+#include "solver/csp-solver-fc.h"
+
+// PRIVATE
+// void print_domains_fc(Domain **domains, size_t num_domains) {
+// 	for (size_t i = 0; i < num_domains; i++) {
+// 		printf("Domain %zu: ", i);
+// 		for (size_t j = 0; j < domains[i]->amount; j++) {
+// 			printf("%zu ", domains[i]->values[j]);
+// 		}
+// 		printf("\n");
+// 	}
+// 	printf("\n");
+// }
+//
+// void print_values(size_t* values, size_t num_domains) {
+// 	for (size_t i = 0; i < num_domains; i++) {
+// 		printf("%zu ", values[i]);
+// 	}
+// 	printf("\n");
+// }
+
+static int backtrack_counter = 0;
+
+bool csp_problem_backtrack_ovars(const CSPProblem *csp,
+ size_t *values, const void *data, size_t index, CSPValueChecklist *checklist,
+ Domain **domains, DomainChange *change_stack, size_t *stack_top
+) {
+	assert(csp_initialised());
+	backtrack_counter++;
+
+	// If all variables are assigned, the CSP is solved
+	if (index == csp_problem_get_num_domains(csp)) {
+		return true;
+	}
+
+	size_t stack_start = *stack_top; // Track the stack size at the start of the call
+
+	// Try all values in the domain of the current variable
+	for (size_t i = 0; i < domains[index]->amount; i++) {
+		// Assign the value to the variable
+		values[index] = domains[index]->values[i];
+
+		// print_values(values, index + 1); //DEBUG
+		// print_domains_fc(domains, csp_problem_get_num_domains(csp)); //DEBUG
+
+		// Check if the assignment is consistent with the constraints
+		if (csp_problem_is_consistent(csp, values, data, index, checklist)
+		 && csp_problem_forward_check(csp, values, data, index, checklist, domains, change_stack, stack_top, *stack_top)
+		 && csp_problem_backtrack_ovars(csp, values, data, index + 1, checklist, domains, change_stack, stack_top)
+		) {
+			return true;
+		}
+
+		// Restore domains from the stack after backtracking
+		while (*stack_top > stack_start) { // Only restore changes made during this call
+			(*stack_top)--;
+			size_t domain_index = change_stack[*stack_top].domain_index;
+			size_t value = change_stack[*stack_top].value;
+
+			domains[domain_index]->values[domains[domain_index]->amount] = value;
+			domains[domain_index]->amount++;
+		}
+		// printf("backtracked\n"); //DEBUG
+		// print_domains_fc(domains, csp_problem_get_num_domains(csp)); //DEBUG
+	}
+	return false;
+}
+
+// PUBLIC
+// Functions
+bool csp_problem_solve_ovars(const CSPProblem *csp, size_t *values, const void *data, CSPValueChecklist *checklist, CSPDataChecklist dataChecklist, size_t* benchmark)
+{
+	assert(csp_initialised());
+
+	size_t num_domains = csp_problem_get_num_domains(csp);
+	Domain *domains[num_domains];
+	size_t stack_capacity = 0; // Adjust as needed
+
+	// Allocate memory for each domain
+	for (size_t i = 0; i < num_domains; i++) {
+		size_t domain_size = csp_problem_get_domain(csp, i);
+		stack_capacity += domain_size;
+		domains[i] = malloc(sizeof(Domain) + domain_size * sizeof(size_t));
+		if (domains[i] == NULL) {
+			perror("malloc");
+			// Free previously allocated domains
+			for (size_t j = 0; j < i; j++) {
+				free(domains[j]);
+			}
+			return false;
+		}
+		domains[i]->amount = domain_size;
+		for (size_t j = 0; j < domain_size; j++) {
+			domains[i]->values[j] = j;
+		}
+	}
+
+	// Initialize the change stack for forward checking
+	DomainChange *change_stack = malloc(stack_capacity * sizeof(DomainChange));
+	if (change_stack == NULL) {
+		perror("malloc");
+		for (size_t i = 0; i < num_domains; i++) {
+			free(domains[i]);
+		}
+		return false;
+	}
+	size_t stack_top = 0;
+
+	reduce_domains(csp, values, data, domains, dataChecklist);
+
+	// Start the backtracking algorithm
+	bool result = csp_problem_backtrack_ovars(csp, values, data, 0, checklist, domains, change_stack, &stack_top);
+
+	// Free allocated memory
+	for (size_t i = 0; i < num_domains; i++) {
+		free(domains[i]);
+	}
+	free(change_stack);
+
+	if (benchmark != NULL) {
+		benchmark[0] = backtrack_counter;
+	}
+
+	// Reset the backtrack counter
+	backtrack_counter = 0;
+
+	return result;
+}
---
+++ csp/src/solver/csp-solver-ovars.h
@@ -0,0 +1,37 @@
+/**
+* @file csp-solver-fc.h
+ * Library CSP solving functions with forward checking
+ *
+ * @author agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
+ * @date 2025
+ * @copyright GNU Lesser General Public License v3.0
+ */
+
+#pragma once
+
+#if !defined (_CSP_H_INSIDE) && !defined (CSP_COMPILATION)
+#error "Only <csp/csp.h> can be included directly."
+#endif
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <core/csp-problem.h>
+#include <core/csp-constraint.h>
+#include <solver/csp-solver.h>
+#include <solver/csp-solver-fc.h>
+
+/** Solve the CSP problem using backtracking.
+ * @param csp The CSP problem to solve.
+ * @param values The values of the variables.
+ * @param data The data to pass to the check function.
+ * @param checklist A pointer to function to get the list of necessary constraints for the current variable.
+ * @param dataChecklist A pointer to function to get the list of necessary data constraints for the current variable.
+ * @param benchmark pointer to Node counter for benchmarking, NULL if no benchmarking required
+ * @return true if the CSP problem is solved, false otherwise.
+ * @pre The csp library is initialised.
+ * @post The values are assigned to the solution.
+ */
+extern bool csp_problem_solve_ovars(const CSPProblem *csp,
+	size_t *values, const void *data, CSPValueChecklist* checklist, CSPDataChecklist dataChecklist, size_t* benchmark);
+
