1ba0f7d8c8b2f84f6972385f2156b61dc6dcbfb6
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-14 16:45:50 +0200

    Added FCOvars

--- csp/btest/cspbtest_plot/graph_sudoku_results.py
+++ csp/btest/cspbtest_plot/graph_sudoku_results.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 SUDOKU_RESULT_FILE = 'sudoku_benchmark.txt'
 SUDOKU_FC_RESULT_FILE = 'sudoku_fc_benchmark.txt'
+SUDOKU_FC_OVARS_RESULT_FILE = 'sudoku_fc_ovars_benchmark.txt'
 SUDOKU_PLOT_FILE = 'sudoku_benchmark.png'
 
 average_count = 5
@@ -11,41 +12,33 @@ unknown_count = [i for i in range(5, 81, 5)]
 nodes = []
 times = []
 
-with open(SUDOKU_RESULT_FILE, 'r') as file:
-    temp_time = []
-    temp_nodes = []
-    for i, line in enumerate(file):
-        time, node = map(float, line.split())
-        temp_time.append(time)
-        temp_nodes.append(node)
-        if len(temp_time) == average_count:
-            times.append(sum(temp_time) / average_count)
-            nodes.append(sum(temp_nodes) / average_count)
-            temp_nodes = []
-            temp_time = []
+def read_sudoku_results(file_name, average_count):
+    times = []
+    nodes = []
+    with open(file_name, 'r') as file:
+        temp_time = []
+        temp_nodes = []
+        for i, line in enumerate(file):
+            time, node = map(float, line.split())
+            temp_time.append(time)
+            temp_nodes.append(node)
+            if len(temp_time) == average_count:
+                times.append(sum(temp_time) / average_count)
+                nodes.append(sum(temp_nodes) / average_count)
+                temp_nodes = []
+                temp_time = []
+    return times, nodes
 
-# Read the data from the second file
-fc_nodes = []
-fc_times = []
-
-with open(SUDOKU_FC_RESULT_FILE, 'r') as file:
-    temp_time = []
-    temp_nodes = []
-    for i, line in enumerate(file):
-        time, node = map(float, line.split())
-        temp_time.append(time)
-        temp_nodes.append(node)
-        if len(temp_time) == average_count:
-            fc_times.append(sum(temp_time) / average_count)
-            fc_nodes.append(sum(temp_nodes) / average_count)
-            temp_nodes = []
-            temp_time = []
+times, nodes = read_sudoku_results(SUDOKU_RESULT_FILE, average_count)
+fc_times, fc_nodes = read_sudoku_results(SUDOKU_FC_RESULT_FILE, average_count)
+fc_ovars_times, fc_ovars_nodes = read_sudoku_results(SUDOKU_FC_OVARS_RESULT_FILE, average_count)
 
 # Plot the time taken
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
-plt.plot(unknown_count, times, marker='o', label='Standard')
-plt.plot(unknown_count, fc_times, marker='o', label='Forward Checking')
+plt.plot(unknown_count, times, marker='o', label='Basic')
+plt.plot(unknown_count, fc_times, marker='o', label='FC')
+plt.plot(unknown_count, fc_ovars_times, marker='o', label='FCOvars')
 plt.title('Sudoku Benchmark: Time Taken')
 plt.xlabel(f'Unknown Count, average over {average_count} sudokus')
 plt.ylabel('Time (seconds)')
@@ -55,8 +48,9 @@ plt.legend()
 
 # Plot the number of backtracks
 plt.subplot(1, 2, 2)
-plt.plot(unknown_count, nodes, marker='o', color='b', label='Standard')
-plt.plot(unknown_count, fc_nodes, marker='o', color='r', label='Forward Checking')
+plt.plot(unknown_count, nodes, marker='o', color='b', label='Basic')
+plt.plot(unknown_count, fc_nodes, marker='o', color='r', label='FC')
+plt.plot(unknown_count, fc_ovars_nodes, marker='o', color='g', label='FCOvars')
 plt.title('Sudoku Benchmark: Nodes Explored')
 plt.xlabel(f'Unknown Count, average over {average_count} sudokus')
 plt.ylabel('Node Count')
