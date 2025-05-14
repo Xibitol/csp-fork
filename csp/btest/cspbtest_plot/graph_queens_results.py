c5da36e293e2c2500c6262435702c6ec73e2c2d6
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-14 20:48:09 +0200

    Added directory handling for input files

--- csp/btest/cspbtest_plot/graph_queens_results.py
+++ csp/btest/cspbtest_plot/graph_queens_results.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+import os
 
-NQUEENS_RESULT_FILE = 'n_queens_benchmark.txt'
-NQUEENS_FC_RESULT_FILE = 'n_queens_fc_benchmark.txt'
-NQUEENS_FC_OVARS_RESULT_FILE = 'n_queens_fc_ovars_benchmark.txt'
-NQUEENS_PLOT_FILE = 'n_queens_benchmark.png'
+NQUEENS_RESULT_DIR = 'n_queens/'
+NQUEENS_PLOT_FILE = 'n_queens/benchmark.png'
 
 # Read the data from the files
 def read_results(file_name):
@@ -23,19 +22,26 @@ def read_results(file_name):
                 n_values_odd.append(len(n_values_odd) * 2 + 5)  # Assuming n starts from 5 and increments by 2
     return n_values_pair, times_pair, backtracks_pair, n_values_odd, times_odd, backtracks_odd
 
-# Read results for both normal and FC
-n_values_pair, times_pair, backtracks_pair, n_values_odd, times_odd, backtracks_odd = read_results(NQUEENS_RESULT_FILE)
-n_values_pair_fc, times_pair_fc, backtracks_pair_fc, n_values_odd_fc, times_odd_fc, backtracks_odd_fc = read_results(NQUEENS_FC_RESULT_FILE)
-n_values_pair_ovars, times_pair_ovars, backtracks_pair_ovars, n_values_odd_ovars, times_odd_ovars, backtracks_odd_ovars = read_results(NQUEENS_FC_OVARS_RESULT_FILE)
-
-# Plot the time taken
 plt.figure(figsize=(10, 10))
+
+for fname in os.listdir(NQUEENS_RESULT_DIR):
+    fpath = os.path.join(NQUEENS_RESULT_DIR, fname)
+    if not os.path.isfile(fpath) or not fname.endswith('.txt'):
+        continue
+    (n_values_pair, times_pair, backtracks_pair, n_values_odd, times_odd,
+     backtracks_odd) = read_results(fpath)
+    version = " ".join(fname.split('_')[:-1])
+    plt.subplot(2, 2, 1)
+    plt.plot(n_values_pair, times_pair, marker='o', label=version)
+    plt.subplot(2, 2, 3)
+    plt.plot(n_values_odd, times_odd, marker='o', label=version)
+    plt.subplot(2, 2, 2)
+    plt.plot(n_values_pair, backtracks_pair, marker='o', label=version)
+    plt.subplot(2, 2, 4)
+    plt.plot(n_values_odd, backtracks_odd, marker='o', label=version)
 
 # Even N: Time
 plt.subplot(2, 2, 1)
-plt.plot(n_values_pair, times_pair, marker='o', label='Normal')
-plt.plot(n_values_pair_fc, times_pair_fc, marker='o', label='FC')
-plt.plot(n_values_pair_ovars, times_pair_ovars, marker='o', label='FC with Ovars')
 plt.title('N-Queens Benchmark: Time Taken, N is even')
 plt.xlabel('N')
 plt.ylabel('Time (seconds)')
@@ -45,9 +51,6 @@ plt.legend()
 
 # Even N: Backtracks
 plt.subplot(2, 2, 2)
-plt.plot(n_values_pair, backtracks_pair, marker='o', color='b', label='Normal')
-plt.plot(n_values_pair_fc, backtracks_pair_fc, marker='o', color='g', label='FC')
-plt.plot(n_values_pair_ovars, backtracks_pair_ovars, marker='o', color='r', label='FC with Ovars')
 plt.title('N-Queens Benchmark: Nodes explored, N is even')
 plt.xlabel('N')
 plt.ylabel('Number of Backtracks')
@@ -57,9 +60,6 @@ plt.legend()
 
 # Odd N: Time
 plt.subplot(2, 2, 3)
-plt.plot(n_values_odd, times_odd, marker='o', label='Normal')
-plt.plot(n_values_odd_fc, times_odd_fc, marker='o', label='FC')
-plt.plot(n_values_odd_ovars, times_odd_ovars, marker='o', label='FC with Ovars')
 plt.title('N-Queens Benchmark: Time Taken, N is odd')
 plt.xlabel('N')
 plt.ylabel('Time (seconds)')
@@ -69,9 +69,6 @@ plt.legend()
 
 # Odd N: Backtracks
 plt.subplot(2, 2, 4)
-plt.plot(n_values_odd, backtracks_odd, marker='o', color='b', label='Normal')
-plt.plot(n_values_odd_fc, backtracks_odd_fc, marker='o', color='g', label='FC')
-plt.plot(n_values_odd_ovars, backtracks_odd_ovars, marker='o', color='r', label='FC with Ovars')
 plt.title('N-Queens Benchmark: Nodes explored, N is odd')
 plt.xlabel('N')
 plt.ylabel('Number of Backtracks')
--- csp/btest/cspbtest_plot/graph_sudoku_results.py
+++ csp/btest/cspbtest_plot/graph_sudoku_results.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
+import os
 
-SUDOKU_RESULT_FILE = 'sudoku_benchmark.txt'
-SUDOKU_FC_RESULT_FILE = 'sudoku_fc_benchmark.txt'
-SUDOKU_FC_OVARS_RESULT_FILE = 'sudoku_fc_ovars_benchmark.txt'
-SUDOKU_PLOT_FILE = 'sudoku_benchmark.png'
+SUDOKU_RESULT_DIR = 'sudoku/'
+SUDOKU_PLOT_FILE = 'sudoku/benchmark.png'
 
 average_count = 5
 
@@ -29,16 +28,28 @@ def read_sudoku_results(file_name, average_count):
                 temp_time = []
     return times, nodes
 
-times, nodes = read_sudoku_results(SUDOKU_RESULT_FILE, average_count)
-fc_times, fc_nodes = read_sudoku_results(SUDOKU_FC_RESULT_FILE, average_count)
-fc_ovars_times, fc_ovars_nodes = read_sudoku_results(SUDOKU_FC_OVARS_RESULT_FILE, average_count)
+plt.figure(figsize=(10, 5))
+
+for fname in os.listdir(SUDOKU_RESULT_DIR):
+    fpath = os.path.join(SUDOKU_RESULT_DIR, fname)
+    if not os.path.isfile(fpath) or not fname.endswith('.txt'):
+        continue
+    times, nodes = read_sudoku_results(fpath, average_count)
+    version = " ".join(fname.split('_')[:-1])
+    plt.subplot(1, 2, 1)
+    plt.plot(unknown_count, times, marker='o', label=version)
+    plt.subplot(1, 2, 2)
+    plt.plot(unknown_count, nodes, marker='o', label=version)
+
+# times, nodes = read_sudoku_results(SUDOKU_RESULT_FILE, average_count)
+# fc_times, fc_nodes = read_sudoku_results(SUDOKU_FC_RESULT_FILE, average_count)
+# fc_ovars_times, fc_ovars_nodes = read_sudoku_results(SUDOKU_FC_OVARS_RESULT_FILE, average_count)
 
 # Plot the time taken
-plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
-plt.plot(unknown_count, times, marker='o', label='Basic')
-plt.plot(unknown_count, fc_times, marker='o', label='FC')
-plt.plot(unknown_count, fc_ovars_times, marker='o', label='FCOvars')
+# plt.plot(unknown_count, times, marker='o', label='Basic')
+# plt.plot(unknown_count, fc_times, marker='o', label='FC')
+# plt.plot(unknown_count, fc_ovars_times, marker='o', label='FCOvars')
 plt.title('Sudoku Benchmark: Time Taken')
 plt.xlabel(f'Unknown Count, average over {average_count} sudokus')
 plt.ylabel('Time (seconds)')
@@ -48,9 +59,9 @@ plt.legend()
 
 # Plot the number of backtracks
 plt.subplot(1, 2, 2)
-plt.plot(unknown_count, nodes, marker='o', color='b', label='Basic')
-plt.plot(unknown_count, fc_nodes, marker='o', color='r', label='FC')
-plt.plot(unknown_count, fc_ovars_nodes, marker='o', color='g', label='FCOvars')
+# plt.plot(unknown_count, nodes, marker='o', color='b', label='Basic')
+# plt.plot(unknown_count, fc_nodes, marker='o', color='r', label='FC')
+# plt.plot(unknown_count, fc_ovars_nodes, marker='o', color='g', label='FCOvars')
 plt.title('Sudoku Benchmark: Nodes Explored')
 plt.xlabel(f'Unknown Count, average over {average_count} sudokus')
 plt.ylabel('Node Count')
