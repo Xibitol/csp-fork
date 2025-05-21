da1c1fb461e08ba279ee27f4cc017eee736123a6
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-21 17:47:53 +0200

    Improved package organization

--- cmake/FindValgrind.cmake
+++ cmake/FindValgrind.cmake
@@ -4,10 +4,3 @@ include(CMakeDependentOption)
 cmake_dependent_option(USE_VALGRIND "Running tests using valgrind" OFF "VALGRIND_EXECUTABLE" OFF)
 mark_as_advanced(USE_VALGRIND)
 
-function(valgrind_test NAME COMMAND)
-  if(USE_VALGRIND)
-    add_test(${NAME}[valgrind] ${VALGRIND_EXECUTABLE} --leak-check=full --quiet --error-exitcode=1 ${COMMAND})
-  endif()
-  add_test(${NAME} ${COMMAND})
-endfunction()
-
